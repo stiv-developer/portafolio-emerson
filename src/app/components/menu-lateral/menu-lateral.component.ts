72304baab573ae2de764d45a3c04ce44bac39d19
stichi-developer <estvrak31@gmail.com>
2021-06-19 18:44:03 -0500

    implementacion de header y navbar

--- src/app/components/menu-lateral/menu-lateral.component.ts
+++ src/app/components/menu-lateral/menu-lateral.component.ts
@@ -3,11 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-lateral',
   templateUrl: './menu-lateral.component.html',
-  styleUrls: ['./menu-lateral.component.css']
+  styleUrls: ['./menu-lateral.component.scss']
 })
 export class MenuLateralComponent implements OnInit {
 
-  constructor() { }
+  showMenu (headerToggle:any, navbarId:any)  {
+    const toggleBtn:any = document.getElementById(headerToggle),
+    nav:any = document.getElementById(navbarId)
+    
+    if(headerToggle && navbarId){
+      nav.classList.toggle('show-menu')
+      toggleBtn.classList.toggle('bx-x')
+    }
+  }
 
   ngOnInit(): void {
   }
